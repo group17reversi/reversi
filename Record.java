2d7096264c6ca1f454eeba858a4567ea4f5f2a7e
99zxn <50001773+99zxn@users.noreply.github.com>
2019-05-28 13:42:36 +0800

    Add files via upload

---
+++ Record.java
@@ -0,0 +1,13 @@
+package reversi;
+
+public class Record {
+	String winner;
+	String mode;
+	String level;
+	public Record(String w,String m, String l) {
+	this.level = l;
+	this.mode = m;
+	this.winner = w;
+	}
+}
+
